8d731afe7c99a71f513e0bd6eb2b0f861918a038
mayankpmahajan <mayankpmahajan@gmail.com>
2025-08-02 17:26:20 +0530

    bug fixes

--- chatApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('signup/', SignUpView.as_view()),
-    path('login/', LoginView.as_view()),
-    path('resetPassword/', ResetPasswordView.as_view()),
-]
--- utils/env.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-from pathlib import Path
-
-# Load environment variables from .env
-def get_env_variable(key: str, default=None):
-    env_path = Path(__file__).resolve().parent.parent / ".env"
-    load_dotenv(dotenv_path=env_path)
-    return os.getenv(key, default)
